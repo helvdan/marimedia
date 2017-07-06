822a98cded0ad71edd1fae1278c51bd4d3c8f0ea
nzvyagin <nzvyagin@public.ru>
2017-07-06 19:21:43 +0300

    Body corrected

--- viewer.py
+++ viewer.py
@@ -4,6 +4,7 @@ import argparse
 import pickle
 from settings import DATA_PATH
 from dateparser import parse
+from lxml import etree
 
 parser = argparse.ArgumentParser(description='Number of articles.')
 parser.add_argument(
@@ -12,20 +13,34 @@ parser.add_argument(
 args = parser.parse_args()
 pubdate = args.date.date()
 
-with open(DATA_PATH, 'r') as data_file:
 
-    output = []
+def get_articles(pubdate):
+    articles = []
 
-    for article in pickle.load(data_file):
-        if article['pubdate'].date() == pubdate:
-            output.append(article)
+    with open(DATA_PATH, 'r') as data_file:
+        for article in pickle.load(data_file):
+            if article['pubdate'].date() == pubdate:
+                articles.append(article)
 
-    if len(output) != 0:
-        print "%s news found" % len(output)
+    return articles
 
-        for article in output:
-            print str(article['pubdate'].strftime("%Y-%m-%d %H:%M")) + '  ' + unicode(article['title'])
-            print unicode(article['link'])
-            print str(article['body'])
-    else:
-        print "Sorry, for " + str(pubdate) + " no news found"
+
+articles = get_articles(pubdate)
+
+if len(articles) != 0:
+    print "%s news found" % len(articles)
+    print '\n\n'
+
+    for article in articles:
+        print str(article['pubdate'].strftime("%Y-%m-%d %H:%M")) + '  ' + unicode(article['title'])
+        print '\n'
+        print unicode(article['link'])
+        print '\n'
+        body = str(article['body'])
+        html = etree.HTML(body.decode('utf-8'))
+        for p in html.xpath(u'//p[not(contains(., "гиперссылка обязательна"))]//text()'):
+            print p
+            print unichr(2029)
+        print '\n\n'
+else:
+    print "Sorry, for " + str(pubdate) + " no news found"
