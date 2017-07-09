7e79c07ac245ab8255bca3ed68f4a8b474d8d5c1
Leonid Makeev <lmakeev@rbc.ru>
2017-07-09 21:42:15 +0300

    refactor viewer OOP

--- viewer.py
+++ viewer.py
@@ -6,41 +6,51 @@ from settings import DATA_PATH
 from dateparser import parse
 from lxml import etree
 
-parser = argparse.ArgumentParser(description='Number of articles.')
-parser.add_argument(
-    '--date', required=True, help='aritcles to be showed for date', type=parse
-)
-args = parser.parse_args()
-pubdate = args.date.date()
+
+class Viewer(object):
+
+    def __init__(self):
+        with open(DATA_PATH, 'r') as data_file:
+            self.articles = pickle.load(data_file)
+
+    def _filter_articles(self, pubdate):
+        return [
+            article
+            for article in self.articles
+            if article['pubdate'].date() == pubdate
+        ]
+
+    def print_articles(self, pubdate, text_length=100):
+        articles = self._filter_articles(pubdate)
+        articles_count = len(articles)
+
+        if not articles_count:
+            print u'Извините, новостей за %s не найдено' % pubdate
+            return
+
+        print u'За %s найдено %s новостей\n' % (pubdate, articles_count)
+        for article in articles:
+            print u'{pubdate} {title}'.format(**article)
+            print article['link']
+            body = str(article['body'])
+            html = etree.HTML(body.decode('utf-8'))
+            for p in html.xpath(u'//p[not(contains(., "гиперссылка обязательна"))]//text()'):
+                if not p:
+                    continue
+
+                while p:
+                    print '\t' + p[:text_length]
+                    p = p[text_length:]
+            print ''
 
 
-def get_articles(pubdate):
-    articles = []
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Number of articles.')
+    parser.add_argument(
+        '--date', required=True, help='aritcles to be showed for date', type=parse
+    )
+    args = parser.parse_args()
+    pubdate = args.date.date()
 
-    with open(DATA_PATH, 'r') as data_file:
-        for article in pickle.load(data_file):
-            if article['pubdate'].date() == pubdate:
-                articles.append(article)
-
-    return articles
-
-
-articles = get_articles(pubdate)
-
-if len(articles) != 0:
-    print "%s news found" % len(articles)
-    print '\n\n'
-
-    for article in articles:
-        print str(article['pubdate'].strftime("%Y-%m-%d %H:%M")) + '  ' + unicode(article['title'])
-        print '\n'
-        print unicode(article['link'])
-        print '\n'
-        body = str(article['body'])
-        html = etree.HTML(body.decode('utf-8'))
-        for p in html.xpath(u'//p[not(contains(., "гиперссылка обязательна"))]//text()'):
-            print p
-            print unichr(2029)
-        print '\n\n'
-else:
-    print "Sorry, for " + str(pubdate) + " no news found"
+    viewer = Viewer()
+    viewer.print_articles(pubdate)
